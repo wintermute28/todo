cca5718d186612feb4925991e4a0e0a7f9860669
wintermute28 <a28c@yandex.ru>
2023-12-20 15:25:19 +0300

    Separate the components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+  </ul>
+  );
+};
 
+const AppHeader = () => {
+  return <h1>My Todo List</h1>;
+};
 
+const SearchPanel = () => {
+  return <input placeholder="Search" />;
+};
+
+const App = () => {
+  return (
+    <>
+    <AppHeader />
+    <SearchPanel />
+    <TodoList />
+    </>
+  )
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-
-  </React.StrictMode>
-);
-
+root.render(<App />);
