2683cd2505d786a583f1dac4d7930da6c5054083
wintermute28 <a28c@yandex.ru>
2023-12-20 18:53:36 +0300

    Use class-components

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,32 +1,36 @@
+import { Component } from 'react';
+
 import './todo-list-item.css';
 
-const TodoListItem = ({ label, important = false }) => {
+export default class TodoListItem extends Component {
 
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
+  render() {
+    const { label, important = false } = this.props;
+
+    const style = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal'
+    };
+  
+    return (
+      <span className="todo-list-item">
+          <span
+          className="todo-list-item-label"
+              style={style}>
+              {label}
+          </span>
+          <div>
+              <button type="button"
+                  className="btn btn-outline-success btn-sm float-right">
+                  <i className="bi bi-exclamation-lg" />
+              </button>
+  
+              <button type="button"
+                  className="btn btn-outline-danger btn-sm float-right">
+                  <i className="bi bi-trash" />
+              </button>
+          </div>
+      </span>
+    );
   };
-
-  return (
-    <span className="todo-list-item">
-        <span
-        className="todo-list-item-label"
-            style={style}>
-            {label}
-        </span>
-        <div>
-            <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
-                <i className="bi bi-exclamation-lg" />
-            </button>
-
-            <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
-                <i className="bi bi-trash" />
-            </button>
-        </div>
-    </span>
-  );
-};
-
-export default TodoListItem;
+}
