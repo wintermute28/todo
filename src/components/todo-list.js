3cb4ecb4ae0ce83b774e46f63893bd9f430074db
wintermute28 <a28c@yandex.ru>
2023-12-20 16:39:56 +0300

    Use array as props

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,12 +1,18 @@
 import TodoListItem from "./todo-list-item";
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
+
+  const elements = todos.map((item) => {
+    return(
+      <li>
+        <TodoListItem { ...item } />
+      </li>
+    )
+  })
+
     return (
       <ul>
-        <li><TodoListItem label="Drink Coffee" /></li>
-        <li><TodoListItem
-          label="Build React App"
-          important /></li>
+        { elements }
     </ul>
     );
   };
