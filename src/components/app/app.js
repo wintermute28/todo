c546c1771d88141b205d2b1e025d4d1f7e5078e0
wintermute28 <a28c@yandex.ru>
2023-12-30 11:01:36 +0300

    Add filter-buttons

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,8 @@ export default class App extends Component {
       this.createTodoItem("Make Awesome App"),
       this.createTodoItem("Have a lunch")
     ],
-    term: ""
+    term: "",
+    filter: "all" //active,all, done,
   };
 
   createTodoItem(label) {
@@ -86,6 +87,10 @@ export default class App extends Component {
     this.setState( {term} );
   };
 
+  onFilterChange = (filter) => {
+    this.setState( {filter} );
+  };
+
   search = (items, term) => {
     if (term.length === 0) {
       return items;
@@ -95,12 +100,28 @@ export default class App extends Component {
             .toLowerCase().indexOf(term.toLowerCase()) > -1;
     })
   };
+
+  filter(items, filter) {
+
+    switch(filter) {
+      case "all":
+        return items;
+      case "active":
+        return items.filter((item) => !item.done);
+      case "done":
+        return items.filter((item) => item.done);
+      default:
+        return items;
+    }
+  }
   
   render() {
 
-    const { todoData, term } = this.state;
+    const { todoData, term, filter } = this.state;
 
-    const visibleItems = this.search(todoData, term);
+    const visibleItems = this.filter(
+      this.search(todoData, term), filter);
+
     const doneCount = todoData
                       .filter((el) => el.done).length;
     const todoCount = todoData.length - doneCount
@@ -110,7 +131,9 @@ export default class App extends Component {
         <div className="top-panel d-flex">
           <SearchPanel 
             onSearchChange={this.onSearchChange}/>
-          <ItemStatusFilter />
+          <ItemStatusFilter
+          filter={filter}
+          onFilterChange={this.onFilterChange} />
         </div>
 
         <TodoList
